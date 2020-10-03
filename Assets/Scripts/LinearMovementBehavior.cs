1872163a8bcf5bf7fb5aa2f1ea519fd6ac732ea8
DawoudSheraz <dawoud.sheraz@gmail.com>
2020-10-03 21:34:37 +0500

    add linear moving platform

---
+++ Assets/Scripts/LinearMovementBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovementBehavior : MonoBehaviour
+{
+    /* Script to move an object to and fro horizontally over a specified distance. */
+
+    public float moveDistance;
+
+    public float moveSpeed;
+
+    [SerializeField]
+    private bool movingRight;
+
+    private float distanceCovered = 0f;
+
+    
+    void FixedUpdate()
+    {
+        if (distanceCovered <= moveDistance)
+        {
+            distanceCovered += 1;
+            transform.position = new Vector2(transform.position.x + ((movingRight ? 1: -1) * moveSpeed), transform.position.y);
+        }
+        else if(distanceCovered > moveDistance)
+        {
+            distanceCovered = 0f;
+            movingRight = !movingRight;
+            
+        }
+        
+    }
+}
