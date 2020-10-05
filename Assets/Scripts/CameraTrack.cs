74d195ddbba355e066eb8582bc173432909edd3a
DawoudSheraz <dawoud.sheraz@gmail.com>
2020-10-05 21:57:02 +0500

    initial camera follow script

---
+++ Assets/Scripts/CameraTrack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+
+    // Transform set to player transform
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").transform;
+    }
+
+    // LateUpdate so that camera update is done after all Update() calls have been made
+    void LateUpdate()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
