48df9a9c86fd9e26c8e8e554fdf736f0cd69227e
DawoudSheraz <dawoud.sheraz@gmail.com>
2020-10-06 20:54:48 +0500

    clamp y position of camera

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -8,6 +8,13 @@ public class CameraTrack : MonoBehaviour
     // Transform set to player transform
     private Transform playerTransform;
 
+    // Minimum & Maximum Y value to clamp camera's y position
+    [SerializeField]
+    private float yMin;
+
+    [SerializeField]
+    private float yMax;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +24,10 @@ public class CameraTrack : MonoBehaviour
     // LateUpdate so that camera update is done after all Update() calls have been made
     void LateUpdate()
     {
-        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+        transform.position = new Vector3(
+            playerTransform.position.x,
+            Mathf.Clamp(playerTransform.position.y, yMin, yMax),
+            transform.position.z
+            );
     }
 }
