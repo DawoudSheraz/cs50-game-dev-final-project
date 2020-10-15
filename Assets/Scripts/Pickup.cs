e8d08bae2d1e1ca6831dde6f29d5e7dc6b82ee6e
DawoudSheraz <dawoud.sheraz@gmail.com>
2020-10-15 21:28:52 +0500

    mushroom pickup

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	public bool isRotating;
+
+	public float pickupScore;
+
+
+	void Update () {
+
+		if (isRotating)
+		{
+			transform.Rotate(0, 5f, 0, Space.World);
+		}
+	}
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+		// trigger coin pickup function if a helicopter collides with this
+		//other.transform.parent.GetComponent<Player>().PickupCoin();
+		Destroy(gameObject);
+	}
+}
