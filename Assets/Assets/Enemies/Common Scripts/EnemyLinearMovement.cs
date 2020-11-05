9f6340fff24e1d24a560d754ceaa777acce1cf92
DawoudSheraz <dawoud.sheraz@gmail.com>
2020-11-05 20:47:44 +0500

    add bat enemy

---
+++ Assets/Assets/Enemies/Common Scripts/EnemyLinearMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLinearMovement : MonoBehaviour
+{
+    /* Script to move an object to and fro horizontally over a specified distance. */
+
+    public float moveDistance;
+
+    public float moveSpeed;
+
+    [SerializeField]
+    private bool movingRight;
+
+    private float distanceCovered = 0f;
+
+    // Determine direction based on the face
+    void Start()
+    {
+        if (!movingRight)
+        {
+            Flip();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (distanceCovered <= moveDistance)
+        {
+            distanceCovered += 1;
+            transform.position = new Vector2(transform.position.x + ((movingRight ? 1 : -1) * moveSpeed), transform.position.y);
+        }
+        else if (distanceCovered > moveDistance)
+        {
+            distanceCovered = 0f;
+            movingRight = !movingRight;
+            Flip();
+        }
+
+    }
+
+    private void Flip()
+    {
+        Vector2 scale = transform.localScale;
+        transform.localScale = new Vector2(-1 * scale.x, scale.y);
+
+    }
+}
--- Assets/Scripts/JumpableEnemy.cs
+++ Assets/Scripts/JumpableEnemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,9 @@ public class JumpableEnemy : MonoBehaviour
     // Invincibilty granted to the player if player does not jump on but collides directly with enemy
     public float invincibilityTime;
 
+    // Score given to user after the enemy kill
+    public float killScore = 0f;
+
     public AudioSource killSound;
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -20,12 +24,26 @@ public class JumpableEnemy : MonoBehaviour
             if (player.IsPlayerJumping() && player.IsPlayerDescending())
             {
                 player.HandleJumpOnEnemy(verticalThrust);
+                player.IncrementScore(killScore);
                 killSound.Play();
 
                 // Disable rendering to give effect that object has been removed
                 GetComponentInParent<SpriteRenderer>().enabled = false;
+
+
+                GameObject objectToDestroy;
+                // Get parent object to destroy or object itself if not parent
+                try
+                {
+                    objectToDestroy = transform.parent.gameObject;
+                }
+                catch (NullReferenceException)
+                {
+                    objectToDestroy = gameObject;
+                }
+
                 // Don't destroy until the audio has played
-                Destroy(transform.parent.gameObject, killSound.clip.length);
+                Destroy(objectToDestroy, killSound.clip.length);
             }
             else if (!player.IsPlayerInvincible())
             {
